7db9af0fed7ff6d9ddf90a2e2e0b3218d8dceb8f
sinsenti <sinsenti73@gmail.com>
2025-07-10 13:32:52 +0300

    Auto-push: 2025-07-10 13:32:52

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,12 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
-    public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
-    }
+  public static boolean isAllPositiveNumbers(List<String> args) {
+    return args!=null &&  args.stream().allMatch(StringUtils::isPositiveNumber);
+
+  }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,11 @@
 package com.epam.utils;
 
 public class StringUtils {
-    public static boolean isPositiveNumber(String str) {
-        //code!
+  public static boolean isPositiveNumber(String str) {
+    try {
+      return Double.parseDouble(str) > 0;
+    } catch (NumberFormatException e) {
+      return false;
     }
+  }
 }
