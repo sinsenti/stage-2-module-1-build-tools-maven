15e117edd588f3d01e7943a922c78bd927007185
sinsenti <sinsenti73@gmail.com>
2025-07-10 13:39:15 +0300

    Auto-push: 2025-07-10 13:39:15

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,9 +2,13 @@ package com.epam.utils;
 
 public class StringUtils {
   public static boolean isPositiveNumber(String str) {
+    if (str == null || str.isEmpty())
+      return false;
     try {
       return Double.parseDouble(str) > 0;
     } catch (NumberFormatException e) {
+      return false;
+    } catch (NullPointerException e) {
       return false;
     }
   }
