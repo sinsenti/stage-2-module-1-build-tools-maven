53f3662b6f5f82370dab3830a0323337a7fbbcd9
sinsenti <sinsenti73@gmail.com>
2025-07-15 09:26:44 +0300

    Auto-push: 2025-07-15 09:26:44

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,9 @@ package com.epam.utils;
 
 public class StringUtils {
   public static boolean isPositiveNumber(String str) {
+    if (str.length() > 1 && str.startsWith("0") && !str.startsWith("0.")) {
+      return false;
+    }
     if (str.trim() == null || str.trim().isEmpty())
       return false;
     if (!str.matches("[0-9]+(\\.[0-9]+)?"))
