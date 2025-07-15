65c3323b71533e5f456c7b5585c39c794b3f004f
sinsenti <sinsenti73@gmail.com>
2025-07-15 09:32:56 +0300

    Auto-push: 2025-07-15 09:32:56

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,20 +1,15 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
   public static boolean isPositiveNumber(String str) {
+    if (str == null) {
+      return false;
+    }
     if (str.length() > 1 && str.startsWith("0") && !str.startsWith("0.")) {
       return false;
     }
-    if (str.trim() == null || str.trim().isEmpty())
-      return false;
-    if (!str.matches("[0-9]+(\\.[0-9]+)?"))
-      return false;
-    try {
-      return Double.parseDouble(str) > 0;
-    } catch (NumberFormatException e) {
-      return false;
-    } catch (NullPointerException e) {
-      return false;
-    }
+    return NumberUtils.isParsable(str) && Double.parseDouble(str) > 0;
   }
 }
